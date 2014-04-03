23c93f4c5661b2f606b95a8c1dd94e1932a1ef8a
venyz <contact@ventsislavzhechev.eu>
2014-04-03 09:51:45 +0200

    Modified update-solr to only process actually existing files.

--- nexlt/translationrepository2nexlt/newjson2tsv.sh
+++ nexlt/translationrepository2nexlt/newjson2tsv.sh
@@ -7,12 +7,16 @@
 # Creted by Mirko Plitt mirko.plitt@autodesk.com
 #
 # Changelog
+# v2.0.1	Modified by Ventsislav Zhechev on 03 Apr 2014
+# Modified to skip the test and ACD_old_test repositories.
+#
 # v2.			Modified by Ventsislav Zhechev on 02 Apr 2014
 # Modified to process all JSON files using json2solr.pl
 #
 # v1.			Modified by Mirko Plitt
 # Initial version
 #
+
 touch /mnt/tr/lastrefresh.new
 
 for js in `find /mnt/tr -name "*json" -newer /mnt/tr/lastrefresh`
--- nexlt/translationrepository2nexlt/update-solr.sh
+++ nexlt/translationrepository2nexlt/update-solr.sh
@@ -1,21 +1,20 @@
+#
+# ©2013–2014 Autodesk Development Sàrl
+#
 # Update the Solr index
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/chs-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/csy-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/cht-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/esp-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/hun-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/jpn-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/plk-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/rus-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/deu-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/fra-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/ita-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/kor-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/ptb-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/fin-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/gre-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/nld-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/ptg-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/rom-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/eng-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
-curl 'http://localhost:8983/solr/update/csv?stream.file=/mnt/tr/ara-passolo-data&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true'
+# 
+# Creted by Mirko Plitt
+#
+# Changelog
+# v2.			Modified by Ventsislav Zhechev on 03 Apr 2014
+# Now we are going through all *-passolo-data files in a loop to simplify the addition of new langauges and avoid pinging Solr for non-existing files.
+#
+# v1.			Modified by Mirko Plitt
+# Initial version
+#
+
+for file in /mnt/tr/*-passolo-data
+do
+	echo "Executing http://localhost:8983/solr/update/csv?stream.file=$file&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true"
+	curl "http://localhost:8983/solr/update/csv?stream.file=$file&escape=\&stream.contentType=text/plain;charset=utf-8&separator=%09&commit=true"
+done
