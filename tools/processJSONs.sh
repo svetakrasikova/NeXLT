a5071f77b3079936b4b4976dee4396f822b9503f
venyz <contact@ventsislavzhechev.eu>
2014-04-03 16:31:35 +0200

    Fixed a bug in processJSONs

--- tools/processJSONs.sh
+++ tools/processJSONs.sh
@@ -14,7 +14,7 @@
 mv /OptiBay/SW_JSONs/tools/product.lst /OptiBay/SW_JSONs/tools/old.product.lst
 curl --user 'ferrotp:2@klopklop' http://lsdata-internal.autodesk.com/svn/jsons/ |sed 's/.*"\(.*\)".*/\1/
 /</d' | sort > /OptiBay/SW_JSONs/tools/product.lst
-cd /OptiBay/SW_JSONs/tools
+cd /OptiBay/SW_JSONs
 comm -23 /OptiBay/SW_JSONs/tools/product.lst /OptiBay/SW_JSONs/tools/old.product.lst | sed 's/^/co http:\/\/lsdata-internal.autodesk.com\/svn\/jsons\//' | xargs -tL 1 svn
 
 
