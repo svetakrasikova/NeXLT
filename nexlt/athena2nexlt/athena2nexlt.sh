b649f73840d437dab1edde86b99c6bc6635775c8
venyz <contact@ventsislavzhechev.eu>
2014-04-15 14:37:13 +0200

    Modernised athena2nexlt

--- nexlt/athena2nexlt/athena2nexlt.sh
+++ nexlt/athena2nexlt/athena2nexlt.sh
@@ -1,16 +1,25 @@
+#!/bin/bash
+#
+# ©2013–2014 Autodesk Development Sàrl
+#
+# Creted by Mirko Plitt
+#
+# Changelog
+# v2.			Modified by Ventsislav Zhechev on 15 Apr 2014
+# Added a #! to make this script a proper executable.
+# Removed unnecessary commands, as AthenaExportMt now produces proper output for Solr.
+#
+# v1.			Modified by Mirko Plitt
+# Initial version
+#
+
 echo "*****************************************************"
 date
-#rm /local/cms/javaDBLink/athena2nexlt/*.out
-rm -f /local/cms/javaDBLink/athena2nexlt/*.csv
-cd /local/cms/javaDBLink/athena2nexlt
-java -cp /local/cms/javaDBLink/bzip2.jar:/local/cms/javaDBLink/opencsv-2.3.jar:/local/cms/javaDBLink/oracle_11203_ojdbc6.jar:/local/cms/javaDBLink AthenaExportMt jdbc:oracle:thin:@oracmsprd1.autodesk.com:1521:CMSPRD1 cmsuser Ten2Four ALL $(date --date yesterday +%Y.%m.%d) $(date +%Y.%m.%d) 0 1
-#bunzip2 /local/cms/javaDBLink/athena2nexlt/*.bz2
-#rm /local/cms/javaDBLink/athena2nexlt/*.mt
-#rm /local/cms/javaDBLink/athena2nexlt/*.tm
-#sed -i 's/""*/"/g' /local/cms/javaDBLink/athena2nexlt/*.csv
-for csv in /local/cms/javaDBLink/athena2nexlt/*.csv
+cd /local/cms/NeXLT/nexlt/athena2nexlt
+rm -f *.csv
+java -cp bzip2.jar:opencsv-2.3.jar:oracle_11203_ojdbc6.jar:. AthenaExportMt jdbc:oracle:thin:@oracmsprd1.autodesk.com:1521:CMSPRD1 cmsuser Ten2Four ALL $(date --date yesterday +%Y.%m.%d) $(date +%Y.%m.%d) 0 1
+for csv in *.csv
 do
-        echo "Indexing $csv"
-#        perl /local/cms/javaDBLink/athena2nexlt/athena2solr.pl $csv > $csv.out
-        curl "http://ec2-54-227-78-139.compute-1.amazonaws.com:8983/solr/update/csv?&separator=%09&commit=true" --data-binary @$csv -H 'Content-type:text/plain; charset=utf-8'
+	echo "Indexing $csv"
+	curl "http://ec2-54-227-78-139.compute-1.amazonaws.com:8983/solr/update/csv?&separator=%09&commit=true" --data-binary @$csv -H 'Content-type:text/plain; charset=utf-8'
 done
