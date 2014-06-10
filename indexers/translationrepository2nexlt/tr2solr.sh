473d50514dd71b09d8729821c454a406531f6a34
venyz <contact@ventsislavzhechev.eu>
2014-06-10 08:59:47 +0200

    Fixed a bug in tr2solr.

--- indexers/translationrepository2nexlt/tr2solr.sh
+++ indexers/translationrepository2nexlt/tr2solr.sh
@@ -7,6 +7,10 @@
 # Creted by Mirko Plitt
 #
 # Changelog
+# v3.1		Modified by Ventsislav Zhechev on 10 Jun 2014
+# Added some output to make the logs more readable.
+# Fixed a bug where the data from a newly added repository might not be indexed immediately.
+#
 # v3.0.3	Modified by Ventsislav Zhechev on 27 May 2014
 # Updated the commands to connect to the SVN repository.
 # Moved the check for new products/repositories after the general repository update.
@@ -30,7 +34,10 @@
 # Initial version
 #
 
+echo "*****************************************************"
+date
 cd /local/cms/NeXLT/indexers/translationrepository2nexlt
+# Update the loca SVN store.
 for D in `ls -d ./*/`
   do
     if [ $D != ./test/ ] && [ $D != ./ACD_old_test/ ]
@@ -43,14 +50,26 @@ for D in `ls -d ./*/`
   done
 
 
+# Check if new SVN repositories have been added.
 mv -f product.lst old.product.lst
 curl --user 'ferrotp:2@klopklop' http://lsdata.autodesk.com/svn/jsons/ |sed 's/.*"\(.*\)".*/\1/
 /</d' | sort -f >product.lst
 comm -23 product.lst old.product.lst | sed 's/^/svn --username ferrotp --password 2@klopklop --non-interactive co http:\/\/lsdata.autodesk.com\/svn\/jsons\//' | xargs -L 1 xargs -t
-
+# Make sure we index the new product, even if the JSON files have old time stamps.
+for product in `comm -23 product.lst old.product.lst |sed 's/\/$//'`
+  do
+		for js  in `find $product -name "*json"`
+      do
+        if [ $product != test ] && [ $product != ACD_old_test ]
+        then
+          echo "Parsing $js - product: $product"
+          ./json2solr.pl $js $product
+        fi
+      done
+	done
 
 touch /var/www/solrUpdate/passolo.lastrefresh.new
-
+# Index all files that have been changed since the last indexing.
 for js in `find . -name "*json" -newer /var/www/solrUpdate/passolo.lastrefresh`
   do
     product=`echo -n $js | sed 's/.\/\([^\/]*\)\/[^\/]*.*\/\([^\/]*\)\/[^\/]*.json/\1/'`
