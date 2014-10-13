4a4e5c72518e92ff43ed2bcdd634d0e6bd8b4744
laeubli <s.laeubli@me.com>
2014-10-13 13:21:16 +0200

    remove static paths; add option to process a single JSON file; add CSV (tab-separated) as an output format; include additional command line arguments

--- tools/parseJSON.pl
+++ tools/parseJSON.pl
@@ -6,6 +6,13 @@
 # Based on json2solr.pl by Mirko Plitt
 #
 # Changelog
+#
+# v1.2		Modified by Samuel Läubli on 13 October 2014
+# Removed static path to Passolo root directory and replaced it with a command line argument (-jsonDir).
+# Added option to parse a single JSON file (using -jsonFile)
+# Added tab-separated CSV as output format (use -format=csv or -format=moses).
+# Added command line arguments for product name (-product) and release (-release).
+# 
 # v1.1.1	Modified by Ventsislav Zhechev on 17 May 2014
 # Added a character limit to source and target strings.
 # Relocated the language renaming that fixes some JSON file contents.
@@ -29,19 +36,33 @@ use Thread::Queue;
 
 use Encode qw/encode/;
 use File::Find;
+use File::Spec;
+use File::Basename;
+use Cwd;
 use IO::Compress::Bzip2;
 
 use JSON::XS;
 use jString;
 
-our ($threads);
+our ($threads, $jsonFile, $jsonDir, $targetDir, $format, $product, $release);
 $threads ||= 8;
+$format ||= 'moses';
+die "Error: -format must be either 'moses' or 'csv'.\n"
+	unless ($format eq 'moses' or $format eq 'csv');
+$targetDir = getcwd()
+	unless defined $targetDir; # use current directory by default
+die "Error: Either a single Passolo JSON file or a directory containing Passolo JSON files must be provided via the -jsonFile or -jsonDir argument, respectively.\n"
+	unless (defined $jsonFile or defined $jsonDir);
+if ($format eq 'csv') {
+	print STDERR "Warning: No product name specified (parameter -product is undefined).\n"
+		unless (defined $product or defined $jsonDir);
+	print STDERR "Warning: No release specified (parameter -release is undefined).\n"
+		unless defined $release;
+}
 
 $| = 1;
 select STDERR;
 $| = 1;
-
-#my %ResTypeDll = ( "4" => "Menu" , "5" => "Dialog" , "6" => "String Table" , "9" => "Accelerator Table" , "11" => "Message Table" , "16" => "Version" , "23" => "HTML", "240" => "DLGINIT" );
 
 my @workers :shared;
 my $fileQueue = new Thread::Queue;
@@ -54,12 +75,25 @@ my $printer = sub {
 	}
 	
 	my $counter = 0;
-	my $out = IO::Compress::Bzip2->new("/OptiBay/SW_JSONs/corpus/corpus.sw.$language.bz2");
-	while (my $data = $languageQueues{$language}->dequeue()) {
-		print $out encode "utf-8", $data->[0];
-		$counter += $data->[1];
+	if ($format eq 'moses') {
+		my $out = IO::Compress::Bzip2->new( File::Spec->catfile($targetDir,"corpus.sw.$language.bz2") );
+		# write data for Moses training
+		while (my $data = $languageQueues{$language}->dequeue()) {
+			print $out encode "utf-8", $data->[0];
+			$counter += $data->[1];
+		}
+		close $out;
+	} else {
+		open( my $out, '>', File::Spec->catfile($targetDir, "corpus.sw.$language.csv") );
+		# write CSV header
+		print $out encode "utf-8", join("\t", 'resource', 'restype', 'enu', $language, 'id', 'product', 'release', 'srclc') . "\n";
+		# write CSV data
+		while (my $data = $languageQueues{$language}->dequeue()) {
+			print $out encode "utf-8", $data->[0];
+			$counter += $data->[1];
+		}
+		close $out;
 	}
-	close $out;
 	print STDERR threads->tid().": Output $counter segments for language $language.\n";
 };
 
@@ -78,14 +112,19 @@ sub printForLanguage {
 }
 
 my $processJSON = sub {
-	while (my $jsonFile = $fileQueue->dequeue()) {
-		my ($product) = $jsonFile =~ m!/OptiBay/SW_JSONs/(\w+)/!;
-
-		print STDERR threads->tid().": Parsing $jsonFile.\n";
+	while (my $currentJSONFile = $fileQueue->dequeue()) {
+		
+		# extract product name from path (if not given as a command line argument)
+		if (defined $jsonDir) {
+			($product) = File::Spec->catfile($jsonDir, $currentJSONFile) =~ m/trunk\/(\w+)\//g
+				unless defined $product;
+		}
+		
+		print STDERR threads->tid().": Parsing $currentJSONFile.\n";
 
 		my $json = new JSON::XS;
 				
-		open my $fh, "<$jsonFile";
+		open my $fh, "<$currentJSONFile";
 		# first parse the initial "["
 		my $done = 0;
 		for (;;) {
@@ -93,7 +132,7 @@ my $processJSON = sub {
 			die "read error: $!" unless defined $charsRead;
 			unless ($charsRead) {
 				$done = 1;
-				print STDERR "1. EOF found for $jsonFile\n";
+				print STDERR "1. EOF found for $currentJSONFile\n";
 				last;
 			}
 			$json->incr_parse($buf);
@@ -111,15 +150,19 @@ my $processJSON = sub {
 					last unless @$data;
 					
 					my $lang = $data->[4];
-					#Fix some bugs in the JSON files.
-					$lang = "esp" if $lang eq "esn";
-					$lang = "eng" if $lang eq "enu";
+						#Fix some bugs in the JSON files.
+						$lang = "esp" if $lang eq "esn";
+						$lang = "eng" if $lang eq "enu";
 					my $src;
 					my $trn;
-#					my $restype;
+					my $resource = $data->[1];
+						$resource =~ s/.*\\(.*)/$1/;
+					my $restype = $data->[8]->[0]->[1];				
 					
 					my $toPrint = "";
 					my $counter = 0;
+					
+					#print Dumper $data;
 					
 					foreach my $str (@{$data->[7]}) {
 						$str = jString->populate(@$str);
@@ -130,10 +173,6 @@ my $processJSON = sub {
 							$str->state_review ||
 							$str->state_readonly ||
 							(!$str->state_translated && !$str->state_pretranslated);
-						
-						#TODO: implement this type of check, without using jStringList.pm
-#						$restype = exists $ResTypeDll{$str->resource->restype} && $str->resource->restype =~ /^\d+$/ ? $ResTypeDll{$str->resource->restype} : $str->resource->restype;
-#						next unless $restype;
 						
 						$src = $str->src_text;
 						$src =~ s/&amp;/\&/g;
@@ -151,7 +190,14 @@ my $processJSON = sub {
 						$trn =~ s/\\\t/\\ \t/g;
 						next if length $trn > 5000;
 						
-						$toPrint .= "$src$trn$product◊÷\n";
+						if ($format eq 'moses') {
+							$toPrint .= "$src$trn$product◊÷\n";
+						} elsif ($format eq 'csv') {
+							# note: product and release are not extracted from the JSON file
+							#       they are provided as command line arguments
+							my @fields = ($resource, $restype, $src, $trn, $str->id, $product, $release, lc $src);
+							$toPrint .= formatCSVRow(@fields);
+						}
 						++$counter;
 					}
 					printForLanguage $lang, $toPrint, $counter if $counter;
@@ -163,7 +209,7 @@ my $processJSON = sub {
 				die "read error: $!" unless defined $charsRead;
 				unless ($charsRead) {
 					$done = 1;
-					print STDERR "2. EOF found for $jsonFile\n";
+					print STDERR "2. EOF found for $currentJSONFile\n";
 					last;
 				}
 				$json->incr_parse($buf); # void context, so no parsing
@@ -187,14 +233,14 @@ my $processJSON = sub {
 				die "read error: $!" unless defined $charsRead;
 				unless ($charsRead) {
 					$done = 1;
-					print STDERR "3. EOF found for $jsonFile\n";
+					print STDERR "3. EOF found for $currentJSONFile\n";
 					last;
 				}
 				$json->incr_parse($buf); # void context, so no parsing
 			}
 		}
 		close $fh;
-		print STDERR threads->tid().": $jsonFile processing finished.\n";
+		print STDERR threads->tid().": $currentJSONFile processing finished.\n";
 		
 		my $filesLeft = $fileQueue->pending();
 		print STDERR threads->tid().": $filesLeft files left to process.\n" if $filesLeft && !($filesLeft % 10);
@@ -202,11 +248,27 @@ my $processJSON = sub {
 	print STDERR threads->tid().": Finished work!\n";
 };
 
+sub formatCSVRow {
+	# @param: array of fields
+	# return
+	my $csvRow = '';
+	foreach my $field (@_) {
+		$field =~ s/\t/ /; #TODO: Better escaping for \t ?
+		$csvRow .= "$field\t"; # fields are separated by \t
+	}
+	chop($csvRow); #remove last comma
+	return $csvRow . "\n";
+}
+
 #Launch the desired number of worker threads.
 @workers = @{shared_clone ([map { scalar threads->create($processJSON) } 1..$threads])};
 
 #Find all files that should be processed.
-find(sub {$fileQueue->enqueue($File::Find::name) if /.json$/}, "/OptiBay/SW_JSONs");
+if ($jsonDir) {
+	find(sub {$fileQueue->enqueue($File::Find::name) if /.json$/}, $jsonDir);
+} else {
+	$fileQueue->enqueue($jsonFile);
+}
 
 #Notify the file worker threads that we’ve finished processing
 $fileQueue->enqueue(undef) foreach 1..$threads;
