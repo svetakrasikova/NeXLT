d386fa48619f2ee9c6974b1a4da6f8f26f810a49
venyz <contact@ventsislavzhechev.eu>
2015-01-23 16:27:51 +0100

    We now take DB user/password as command line parameters.

--- indexers/athena2nexlt/athena2nexlt.sh
+++ indexers/athena2nexlt/athena2nexlt.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #
-# ©2013–2014 Autodesk Development Sàrl
+# ©2013–2015 Autodesk Development Sàrl
 #
 # Creted by Mirko Plitt
 #
 # Changelog
+# v2.1		Modified by Ventsislav Zhechev on 23 Jan 2015
+# We now take DB user/password as command line parameters.
+#
 # v2.0.5	Modified by Ventsislav Zhechev on 05 Jun 2014
 # Made sure the Java tool has enough heap memory to run properly.
 #
@@ -28,9 +31,16 @@
 # Initial version
 #
 
+# make sure database user and password are supplied as positional arguments
+if (( "$#" != 2 )) 
+then
+	echo "Usage: athena2nexlt.sh DBUserName DBPassword"
+	exit 1
+fi
+
 echo "*****************************************************"
 date
 cd /local/cms/NeXLT/indexers/athena2nexlt
-java -cp bzip2.jar:oracle_11203_ojdbc6.jar:httpclient-4.3.3.jar:httpcore-4.3.2.jar:commons-logging-1.1.3.jar:json-simple-1.1.1.jar:. -Xmx4096m AthenaExportMt ALL $(date --date yesterday +%Y.%m.%d) $(date +%Y.%m.%d) 0 1 1
+java -cp bzip2.jar:oracle_11203_ojdbc6.jar:httpclient-4.3.3.jar:httpcore-4.3.2.jar:commons-logging-1.1.3.jar:json-simple-1.1.1.jar:. -Xmx4096m AthenaExportMt jdbc:oracle:thin:@oracmsprd1.autodesk.com:1521:CMSPRD1 $1 $2 ALL $(date --date yesterday +%Y.%m.%d) $(date +%Y.%m.%d) 0 1 1
 
 touch /var/www/solrUpdate/athena.lastrefresh
